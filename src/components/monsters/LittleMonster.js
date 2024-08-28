e44efb2ee0457e2184c31bb04f94abff04615bf2
Carlos Daniel <159942296+CarloosDaniel@users.noreply.github.com>
2024-08-28 16:47:53 -0300

    Add files via upload

---
+++ src/components/monsters/LittleMonster.js
@@ -0,0 +1,9 @@
+class littleMonster extends Character{
+    constructor() {
+        super('Little Monster')
+        this.life = 80
+        this.attack = 15
+        this.defense = 3
+        this.maxLife = this.life 
+    }
+}
---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+let char = new Knight('Jonh')
+let monster = new littleMonster()
+
+console.log(char.name)
+
+console.log(monster.name)
